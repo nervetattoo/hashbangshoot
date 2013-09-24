2afd9a35193da09a50404ca16e8dfad25eaf3197
Raymond Julin <raymond.julin@gmail.com>
2013-09-24 20:10:39 +0200

    Working scroller + map generation

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    connect: {
+      dev: {
+        options: {
+          port: 9004,
+          base: './',
+          keepalive: true,
+        }
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
